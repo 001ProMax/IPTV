1b9738f3d1c46e6392a9b0ac9f518f071b434ed4
001ProMax <101964350+001ProMax@users.noreply.github.com>
2024-11-21 15:45:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ liveurls/bilibili.go
@@ -0,0 +1,78 @@
+// Package liveurls
+// @Time:2023/02/10 01:03
+// @File:bilibili.go
+// @SoftWare:Goland
+// @Author:feiyang
+// @Contact:TG@feiyangdigital
+
+package liveurls
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tidwall/gjson"
+	"io"
+	"net/http"
+)
+
+type BiliBili struct {
+	Rid      string
+	Line     string
+	Quality  string
+	Platform string
+}
+
+func (b *BiliBili) GetRealRoomID() any {
+	var firstmap = make(map[string]any)
+	var realroomid string
+	apiurl := "https://api.live.bilibili.com/room/v1/Room/room_init?id=" + b.Rid
+	client := &http.Client{}
+	r, _ := http.NewRequest("GET", apiurl, nil)
+	r.Header.Add("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1")
+	resp, _ := client.Do(r)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	json.Unmarshal(body, &firstmap)
+	if firstmap["msg"] == "直播间不存在" {
+		return nil
+	}
+	if newmap, ok := firstmap["data"].(map[string]any); ok {
+		if newmap["live_status"] != float64(1) {
+			return nil
+		} else {
+			if flt, ok := newmap["room_id"].(float64); ok {
+				realroomid = fmt.Sprintf("%v", int(flt))
+			}
+		}
+
+	}
+	return realroomid
+}
+
+func (b *BiliBili) GetPlayUrl() any {
+	var roomid string
+	var realurl string
+	if str, ok := b.GetRealRoomID().(string); ok {
+		roomid = str
+	} else {
+		return nil
+	}
+	client := &http.Client{}
+	params := map[string]string{
+		"cid":  roomid,
+		"qn":       b.Quality,
+		"platform": b.Platform,
+	}
+	r, _ := http.NewRequest("GET", "https://api.live.bilibili.com/room/v1/Room/playUrl", nil)
+	q := r.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	r.URL.RawQuery = q.Encode()
+	resp, _ := client.Do(r)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var json = string(body)
+	value := gjson.Get(json, "data.durl.0.url")
+	return value
+}
